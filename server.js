947524e7ab568b8ecf1e3409d571076e8403e41a
LocNgoXuan23 <>
2022-01-03 16:17:12 +0700

    initial commit

--- server.js
+++ server.js
@@ -54,9 +54,9 @@ mongoose.connect(URI, {
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
-    // app.get('*', (req, res)=>{
-    //     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    // })
+    app.get('*', (req, res)=>{
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
 }
 
 
